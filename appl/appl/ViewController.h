18b49a4fe1ecd0c50c938beabc47c923270f94d6
theadnan <adnankicin92@gmail.com>
2016-10-23 18:42:03 +0200

    init

---
+++ appl/appl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appl
+//
+//  Created by Reuf Kicin on 10/22/16.
+//  Copyright © 2016 Kicin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ appl/appl/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  appl
+//
+//  Created by Reuf Kicin on 10/22/16.
+//  Copyright © 2016 Kicin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)zbir:(id)sender {
+    int a=1+2;
+    
+}
+
+
+@end
